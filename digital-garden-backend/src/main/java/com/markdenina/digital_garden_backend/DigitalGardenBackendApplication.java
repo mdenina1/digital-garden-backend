5cf1f28cb54b014d3b49a35f556d8ee6c6402d47
Mark Denina <madenina354@gmail.com>
2025-05-22 15:25:53 -0400

    Initial Spring Boot

---
+++ digital-garden-backend/src/main/java/com/markdenina/digital_garden_backend/DigitalGardenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.markdenina.digital_garden_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGardenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalGardenBackendApplication.class, args);
+	}
+
+}
---
+++ digital-garden-backend/src/test/java/com/markdenina/digital_garden_backend/DigitalGardenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markdenina.digital_garden_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalGardenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
